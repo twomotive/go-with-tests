ee27e1fa373aee01d2f67b7d9a34c43d34c9e6ec
twomotive <112391727+twomotive@users.noreply.github.com>
2024-01-25 13:15:19 +0300

    stage 1 completed

--- hello.go
+++ hello.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
-
-func Hello() string {
-	return "Hello, world"
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
 }
