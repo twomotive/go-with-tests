430d2b2380c842f9e8c9654e09e0f37d6602c480
twomotive <112391727+twomotive@users.noreply.github.com>
2024-01-25 13:57:59 +0300

    completed helloworld

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -1,13 +1,34 @@
 package main
 
-const englishHelloPrefix = "Hello, "
+import "fmt"
 
-func Hello(name string) string {
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+// Hello returns a personalised greeting in a given language.
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
+	fmt.Println(Hello("world", ""))
 }
